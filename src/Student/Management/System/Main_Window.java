bc08fb4b99d4a3d8b0bc90df0452d33e1b472c96
SalajChoudhary19 <salajchoudhary19@gmail.com>
2024-11-07 17:07:58 +0530

    changes in Main file

--- src/Student/Management/System/Main_Window.java
+++ src/Student/Management/System/Main_Window.java
@@ -1,12 +1,80 @@
 package Student.Management.System;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main_Window extends JFrame {
 
     public Main_Window() {
-        setSize(500, 500);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/educational-background-32fehu59qh64u5rc.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1800, 940, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel img = new JLabel(i3);
+        add(img);
+
+        JMenuBar mb = new JMenuBar();
+
+        //new info
+
+        JMenu newInfo = new JMenu("New Information");
+        newInfo.setForeground(Color.black);
+        mb.add(newInfo);
+
+        JMenuItem studentInfo = new JMenuItem("Student Info");
+        studentInfo.setForeground(Color.black);
+        newInfo.add(studentInfo);
+
+        //details
+
+        JMenu newDetails = new JMenu("View Details");
+        newInfo.setForeground(Color.black);
+        mb.add(newDetails);
+
+        JMenuItem studentDetails = new JMenuItem("Student Details");
+        studentInfo.setForeground(Color.black);
+        newDetails.add(studentDetails);
+
+        //leave
+
+        JMenu newLeave = new JMenu("Apply for Leave");
+        newLeave.setForeground(Color.black);
+        mb.add(newLeave);
+
+        JMenuItem studentLeave = new JMenuItem("Student Leave Form");
+        studentLeave.setForeground(Color.black);
+        newLeave.add(studentLeave);
+
+        //Exams
+
+        JMenu newExams = new JMenu("Exam Details");
+        newExams.setForeground(Color.black);
+        mb.add(newExams);
+
+        JMenuItem TimetableExams = new JMenuItem("Exam TimeTable");
+        TimetableExams.setForeground(Color.black);
+        newExams.add(TimetableExams);
+
+        JMenuItem SyllabusExams = new JMenuItem("Exam Syllabus");
+        SyllabusExams.setForeground(Color.black);
+        newExams.add(SyllabusExams);
+
+        //Results
+
+        JMenu newResult = new JMenu("Result");
+        newResult.setForeground(Color.black);
+        mb.add(newResult);
+
+        JMenuItem studentResult = new JMenuItem("Student Results");
+        studentResult.setForeground(Color.black);
+        newLeave.add(studentResult);
+
+        setJMenuBar(mb);
+
+        setSize(1800, 940);
+        setLocationRelativeTo(null);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public static void main(String[] args) {
         new Main_Window();
